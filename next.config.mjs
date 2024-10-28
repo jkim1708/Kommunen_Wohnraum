58382841408bdb5dbc4a9cbe8c1fb32223d4e724
jkim1708 <jkim1708@gmail.com>
2024-10-28 14:36:57 +0100

    unopt

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-                port: '', // Leave empty if not using a specific port
-                pathname: '/',
-            },
-        ],
+       unoptimized: true,
     },
 };
 
